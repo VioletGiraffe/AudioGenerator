48db801b8a5649cf8de4f89d92f3797c533f0813
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-07-04 21:04:54 +0300

    WaveformGenerator: high DPI scaling enabled

--- WaveformGenerator/main.cpp
+++ WaveformGenerator/main.cpp
@@ -3,7 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
 	QApplication a(argc, argv);
+
 	MainWindow w;
 	w.show();
 
