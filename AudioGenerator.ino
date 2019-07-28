28c38ed4d6e9a61cdfa707815aa3d3383cd4d829
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-07-28 19:45:25 +0300

    DAC working

--- AudioGenerator.ino
+++ AudioGenerator.ino
@@ -46,17 +46,9 @@ constexpr uint32_t samplingRate = 24000;
 
 //static ButtonHandler button(BUTTON_PIN, ButtonHandler::NormalOpen);
 
-void playSound()
-{
-	dac_write(soundGen.nextSample<CWaveformSin, samplingRate>(1000u));
-}
-
-// void rotationListener(void*, RotationDirection direction)
-// {
-// 	display.fillScreen(color<0, 0, 0>);
-// 	display.setCursor(0, 0);
-// 	display.print(encoder.counterValue());
-// }
+constexpr uint32_t sinsize = 1024;
+uint32_t sinus[2][sinsize];
+volatile uint32_t bufn = 1;
 
 void setup()
 {
@@ -69,29 +61,76 @@ void setup()
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, LOW);
 
-	dac_setup();
+	for (int i = 0; i < sinsize; ++i)
+	{
+		constexpr uint32_t chsel = 1 << 28;					// LSB on DAC0, MSB on DAC1 !!
+		sinus[0][i] = 2047.0f * sin((i * 2) * (float)PI / sinsize) + 2047.0f;  //  0 < sinus [i] < 4096
+		sinus[1][i] = sinus[0][i] |= sinus[0][i] << 16 | chsel; // two buffers formated
+																// MSB [31:16]on channel 1
+																// LSB [15:0] on chanel 0
+	}
 
-	//Timer3.attachInterrupt(&playSound).setFrequency(samplingRate).start(); // 24 kHz sampling rate
+	tc_setup();
+	dac_setup();
 }
 
 void dac_setup()
 {
-	pmc_enable_periph_clk(DACC_INTERFACE_ID); // start clocking DAC
-	DACC->DACC_CR = DACC_CR_SWRST;  // reset DAC
+	PMC->PMC_PCER1 = PMC_PCER1_PID38; // DACC power ON
+	DACC->DACC_CR = DACC_CR_SWRST; // Reset DACC
 
-	DACC->DACC_MR =
-		0 /* No trigger, else DACC_MR_TRGEN_EN */ | DACC_MR_TRGSEL(0) |
-		(0 << DACC_MR_USER_SEL_Pos) |  // select channel 0
-		DACC_MR_REFRESH(0x2) |        // Refresh = DAC_CLOCK / (1024 * Fs) (https://cjpnmiscellany.wordpress.com/2015/08/20/settings-for-the-arduino-dac/)
-		(24 << DACC_MR_STARTUP_Pos);  // 24 = 1536 cycles which I think is in range 23..45us since DAC clock = 42MHz
+	DACC->DACC_MR = DACC_MR_TRGEN_EN		// Hardware trigger select
+					| DACC_MR_TRGSEL(0b010) // Trigger by TIOA2
+					| DACC_MR_TAG_EN		// enable TAG to set channel in CDR
+					| DACC_MR_WORD_WORD		// write to both channels
+					| DACC_MR_REFRESH(1) | DACC_MR_STARTUP_8 | DACC_MR_MAXS;
 
-	DACC->DACC_IDR = 0xFFFFFFFF; // no interrupts
-	DACC->DACC_CHER = DACC_CHER_CH0 << 0; // enable chan0
+	DACC->DACC_IER |= DACC_IER_TXBUFE; // Interrupt used by PDC DMA
+
+	DACC->DACC_ACR = DACC_ACR_IBCTLCH0(0b10) // For DAC frequency > 500 KHz
+					 | DACC_ACR_IBCTLCH1(0b10) | DACC_ACR_IBCTLDACCORE(0b01);
+
+	NVIC_EnableIRQ(DACC_IRQn); // Enable DACC interrupt
+
+	DACC->DACC_CHER = DACC_CHER_CH0	// enable channel 0 = DAC0
+					  | DACC_CHER_CH1; // enable channel 1 = DAC1
+
+	/*************   configure PDC/DMA  for DAC *******************/
+
+	DACC->DACC_TPR = (uint32_t)sinus[0]; // DMA buffer
+	DACC->DACC_TCR = sinsize;
+	DACC->DACC_TNPR = (uint32_t)sinus[1]; // next DMA buffer (circular buffer)
+	DACC->DACC_TNCR = sinsize;
+	bufn = 1;
+	DACC->DACC_PTCR = DACC_PTCR_TXTEN; // Enable PDC Transmit channel request
 }
 
-inline void dac_write(int val)
+void DACC_Handler()
 {
-	DACC->DACC_CDR = val & 0xFFF;
+	const auto status = DACC->DACC_ISR; // Read and save DAC status register
+	//if (status & DACC_ISR_TXBUFE)
+	{ // move DMA pointer to next buffer
+		bufn = 1u - bufn;
+		DACC->DACC_TNPR = (uint32_t)sinus[bufn];
+		DACC->DACC_TNCR = sinsize;
+	}
+}
+
+void tc_setup()
+{
+	PMC->PMC_PCER0 |= PMC_PCER0_PID28;					   // TC2 power ON : Timer Counter 0 channel 2 IS TC2
+	TC0->TC_CHANNEL[1].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK1 // MCK/8, clk on rising edge
+								| TC_CMR_WAVE			   // Waveform mode
+								| TC_CMR_WAVSEL_UP_RC	  // UP mode with automatic trigger on RC Compare
+								| TC_CMR_ACPA_CLEAR		   // Clear TIOA2 on RA compare match
+								| TC_CMR_ACPC_SET;		   // Set TIOA2 on RC compare match
+
+	//constexpr uint32_t targetSamplingRate = 500000;
+	//constexpr uint32_t divider = F_CPU / 8 / targetSamplingRate;
+	TC0->TC_CHANNEL[1].TC_RC = 2; //<*********************  Frequency = (Mck/8)/TC_RC = 44.1 KHz
+	TC0->TC_CHANNEL[1].TC_RA = 1;  //<********************   Any Duty cycle in between 1 and TC_RC
+
+	TC0->TC_CHANNEL[1].TC_CCR = TC_CCR_SWTRG | TC_CCR_CLKEN; // Software trigger TC2 counter and enable
 }
 
 void loop()
